760237c2262c2da140914e7dbabd847076669f42
MohammedGhazaly <warendragon67@gmail.com>
2023-12-18 16:11:50 +0200

    Finished compressing selected images

--- lib/core/firebase_services/firebase_services.dart
+++ lib/core/firebase_services/firebase_services.dart
@@ -4,7 +4,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:drivey_files/core/utils/app_values.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:mime/mime.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 class FirebaseService {
   static Future<void> uploadFolder({required String name}) async {
@@ -26,23 +29,47 @@ class FirebaseService {
 
   static uploadFile({required String folderName}) async {
     List<File> files = [];
-    List<String?> fileTypes = [];
     FilePickerResult? result =
         await FilePicker.platform.pickFiles(allowMultiple: true);
+
+    // Getting the file paths and adding them to List
     if (result != null) {
       for (var path in result.paths) {
         files.add(File(path!));
       }
 
+      // Getting filetype, fileName and extension
       for (var file in files) {
         String? fileType = lookupMimeType(file.path);
-        fileTypes.add(fileType?.split("/")[0]);
+        // Getting filetype for example image
+        String? filteredFileType = fileType!.split("/")[0];
+
         List<String?> pathParts = file.path.split("/");
         String? fileName = pathParts.last;
         String? extenstion = fileName?.split(".").last;
-        print(fileName);
-        print(extenstion);
+        File compressedFile = await compressImage(file, filteredFileType);
+        if (filteredFileType == "image") {
+          print("sssssssssss");
+          print(compressedFile);
+        }
+        // Getting compressed Files
+        // print(compressedFile);
       }
     }
+  }
+
+  // Compress files function
+  static Future<File> compressImage(File file, String fileType) async {
+    Uuid uuid = Uuid();
+    String randomStrng = uuid.v4();
+
+    Directory directory = await getTemporaryDirectory();
+    String targetPath = directory.path + "/${randomStrng}.jpg";
+    XFile? result = await FlutterImageCompress.compressAndGetFile(
+        file.path, targetPath,
+        quality: 70);
+    return File(result!.path);
+
+    // else if (fileType == "video") {}
   }
 }
