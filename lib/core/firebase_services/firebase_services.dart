6a25b6d075cd1cf8d7cf9a6fdf942226a094b871
MohammedGhazaly <warendragon67@gmail.com>
2023-12-18 19:41:39 +0200

    Finished uploading files to firebase firestorage

--- lib/core/firebase_services/firebase_services.dart
+++ lib/core/firebase_services/firebase_services.dart
@@ -5,7 +5,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:drivey_files/core/utils/app_values.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:get/get.dart';
 import 'package:mime/mime.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
@@ -49,42 +51,81 @@ class FirebaseService {
         List<String?> pathParts = file.path.split("/");
         String? fileName = pathParts.last;
         String? extenstion = fileName?.split(".").last;
+        late File compressedFile;
+
         if (filteredFileType == "image") {
-          File compressedImage = await compressImage(file);
-          print("IMAGE");
-          print(compressedImage);
+          compressedFile = await compressImage(file);
         } else if (filteredFileType == "video") {
-          File compressedVideo = await compressVideo(file);
-          log("VIDEO");
-          log(compressedVideo.path);
+          compressedFile = await compressVideo(file);
+        } else {
+          compressedFile = file;
         }
-        // Getting compressed Files
-        // print(compressedFile);
+
+        int length = await AppValues.userCollection
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .collection("files")
+            .get()
+            .then((value) => value.docs.length);
+        //  Uploading file to storage
+        log("FileType: ${fileType}");
+        log("filteredFileType: ${filteredFileType}");
+        log("pathParts: ${pathParts}");
+        log("fileName: ${fileName}");
+        log("extenstion: ${extenstion}");
+        log("compressedFile: ${compressedFile}");
+        Get.showOverlay;
+
+        uploadToFireStorage(
+            file: compressedFile, length: length, type: fileType);
       }
     }
   }
 
   // Compress files function
   static Future<File> compressImage(File file) async {
-    Uuid uuid = Uuid();
-    String randomStrng = uuid.v4();
+    try {
+      Uuid uuid = Uuid();
+      String randomStrng = uuid.v4();
 
-    Directory directory = await getTemporaryDirectory();
+      Directory directory = await getTemporaryDirectory();
 
-    String targetPath = directory.path + "/${randomStrng}.jpg";
-    XFile? result = await FlutterImageCompress.compressAndGetFile(
-        file.path, targetPath,
-        quality: 50);
-    return File(result!.path);
+      String targetPath = directory.path + "/${randomStrng}.jpg";
+      XFile? result = await FlutterImageCompress.compressAndGetFile(
+          file.path, targetPath,
+          quality: 50);
+      return File(result!.path);
+    } catch (e) {
+      return file;
+    }
 
     // else if (fileType == "video") {}
   }
 
   static Future<File> compressVideo(File file) async {
-    MediaInfo? info = await VideoCompress.compressVideo(file.path,
-        quality: VideoQuality.MediumQuality,
-        deleteOrigin: false,
-        includeAudio: true);
-    return info!.file!;
+    try {
+      MediaInfo? info = await VideoCompress.compressVideo(file.path,
+          quality: VideoQuality.MediumQuality,
+          deleteOrigin: false,
+          includeAudio: true);
+      return info!.file!;
+    } catch (e) {
+      return file;
+    }
+  }
+
+  static Future<void> uploadToFireStorage(
+      {required File file, required int length, required String type}) async {
+    UploadTask uploadTask = FirebaseStorage.instance
+        .ref()
+        .child("files")
+        .child(FirebaseAuth.instance.currentUser!.uid)
+        .child("File $length")
+        .putFile(file, SettableMetadata(contentType: type));
+    // uploadTask.snapshotEvents.listen((event) {
+    //   print(object);
+    // });
+    TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
+
+    String fileUrl = await taskSnapshot.ref.getDownloadURL();
   }
 }
