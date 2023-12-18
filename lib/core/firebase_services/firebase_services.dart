4faa43c903035efb5b7b6e6fcad08c6ae20c8850
MohammedGhazaly <warendragon67@gmail.com>
2023-12-18 11:32:44 +0200

    Finished extracting fille name and extenstions from filepicker results

--- lib/core/firebase_services/firebase_services.dart
+++ lib/core/firebase_services/firebase_services.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:drivey_files/core/utils/app_values.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mime/mime.dart';
 
 class FirebaseService {
   static Future<void> uploadFolder({required String name}) async {
@@ -22,7 +25,24 @@ class FirebaseService {
   }
 
   static uploadFile({required String folderName}) async {
+    List<File> files = [];
+    List<String?> fileTypes = [];
     FilePickerResult? result =
         await FilePicker.platform.pickFiles(allowMultiple: true);
+    if (result != null) {
+      for (var path in result.paths) {
+        files.add(File(path!));
+      }
+
+      for (var file in files) {
+        String? fileType = lookupMimeType(file.path);
+        fileTypes.add(fileType?.split("/")[0]);
+        List<String?> pathParts = file.path.split("/");
+        String? fileName = pathParts.last;
+        String? extenstion = fileName?.split(".").last;
+        print(fileName);
+        print(extenstion);
+      }
+    }
   }
 }
