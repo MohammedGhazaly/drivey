de7e34360f4bb83f385ecdffa68585118a6a465e
MohammedGhazaly <warendragon67@gmail.com>
2023-12-20 21:48:21 +0200

    Finished viewing images

---
+++ lib/screens/view_file_screen.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:drivey_files/core/utils/app_styles.dart';
+import 'package:drivey_files/models/file_model.dart';
+import 'package:drivey_files/widgets/display_files_screen/download_remove_bottom_sheet.dart';
+import 'package:flutter/material.dart';
+
+class ViewFileScreen extends StatelessWidget {
+  final FileModel file;
+  const ViewFileScreen({super.key, required this.file});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.white,
+        title: Text(
+          file.name,
+          style: AppStyles.textStyle(
+            fontSize: 14,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return DownloadRemoveBottomSheet(fileName: file.name);
+                  });
+            },
+          )
+        ],
+      ),
+      body: file.type == "image"
+          ? Center(
+              child: CachedNetworkImage(
+                imageUrl: file.url,
+                fit: BoxFit.cover,
+              ),
+            )
+          : Container(),
+    );
+  }
+}
--- lib/widgets/display_files_screen/files_grid_view.dart
+++ lib/widgets/display_files_screen/files_grid_view.dart
@@ -1,8 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:drivey_files/core/utils/app_styles.dart';
 import 'package:drivey_files/models/file_model.dart';
+import 'package:drivey_files/screens/view_file_screen.dart';
 import 'package:drivey_files/widgets/display_files_screen/download_remove_bottom_sheet.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class FilesGridView extends StatelessWidget {
   final List<FileModel> files;
@@ -17,71 +19,68 @@ class FilesGridView extends StatelessWidget {
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2, crossAxisSpacing: 12, mainAxisSpacing: 12),
           itemBuilder: (context, index) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16),
-                    child: files[index].type == "image"
-                        ? CachedNetworkImage(
-                            fit: BoxFit.cover,
-                            imageUrl: files[index].url,
-                          )
-                        : Image.asset(
-                            "assets/images/${files[index].extenstion}.png",
-                            fit: BoxFit.cover,
-                            // width: 75,
-                          ),
-                  ),
-                ),
-                // Expanded(
-                //   child: ClipRRect(
-                //     borderRadius: BorderRadius.circular(16),
-                //     child: Image.asset(
-                //       "assets/images/doc.png",
-                //       fit: BoxFit.cover,
-                //       // width: 75,
-                //     ),
-                //   ),
-                // ),
-                SizedBox(
-                  height: 6,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        files[index].name,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: AppStyles.textStyle(
-                            fontSize: 12,
-                            color: Colors.black,
-                            fontWeight: FontWeight.w500),
-                      ),
+            return InkWell(
+              onTap: () {
+                Get.to(ViewFileScreen(file: files[index]),
+                    transition: Transition.zoom);
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: files[index].type == "image"
+                          ? CachedNetworkImage(
+                              fit: BoxFit.cover,
+                              imageUrl: files[index].url,
+                            )
+                          : Image.asset(
+                              "assets/images/${files[index].extenstion}.png",
+                              fit: BoxFit.cover,
+                              // width: 75,
+                            ),
                     ),
-                    InkWell(
-                      overlayColor: MaterialStateProperty.resolveWith((states) {
-                        return Colors.transparent;
-                      }),
-                      onTap: () {
-                        showModalBottomSheet(
-                            context: context,
-                            builder: (context) {
-                              return DownloadRemoveBottomSheet(
-                                fileName: files[index].name.split(".")[0],
-                              );
-                            });
-                      },
-                      child: Icon(
-                        Icons.more_vert,
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          files[index].name,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: AppStyles.textStyle(
+                              fontSize: 12,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500),
+                        ),
                       ),
-                    )
-                  ],
-                )
-              ],
+                      InkWell(
+                        overlayColor:
+                            MaterialStateProperty.resolveWith((states) {
+                          return Colors.transparent;
+                        }),
+                        onTap: () {
+                          showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return DownloadRemoveBottomSheet(
+                                  fileName: files[index].name.split(".")[0],
+                                );
+                              });
+                        },
+                        child: Icon(
+                          Icons.more_vert,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             );
           }),
     );
